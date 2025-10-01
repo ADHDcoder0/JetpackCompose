22777713411eb01ef3c6e801e37c6a0b2e19a880
Kushagra <kushagra.collegestuff@gmail.com>
2025-10-01 17:40:06 +0530

    Kya hua

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -6,15 +6,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -22,17 +31,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column(modifier =  Modifier
-                .fillMaxSize()
-                .background(Color.Green),
-                horizontalAlignment = Alignment.CenterHorizontally
-
+            Row(
+                modifier = Modifier
+                    .fillMaxSize(0.99f)
+//                .width(1920.dp)
+//                .height(3600.dp)
+//                .fillMaxHeight(0.7f)
+//                .fillMaxWidth(0.6f)
+                    .background(Color.Red)
+                    .requiredWidth(300.dp)
+                    .padding(21.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
             ) {
-                Text("Hello")
+                Text("Hello", modifier = Modifier.offset(50.dp, 20.dp)
+                    .background(Color.Blue))
                 Text("world")
+                Text("Why")
             }
         }
-    }
-}
+    }}
 
 
